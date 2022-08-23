08445ae2f82bbd6b3a3a9f97cdb889e821f5b340
Simon <ad7588@coventry.ac.uk>
2022-08-23 16:47:45 +0100

    changes to navbar and footer

--- header.php
+++ header.php
@@ -45,36 +45,31 @@
 
           <div class="container">
 
-            <div class="row">
               <!-- Navbar Brand -->
+              <h1><a class="navbar-brand md d-none d-md-block" href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a></h1>
               <a class="navbar-brand xs d-md-none" href="<?php echo esc_url(home_url()); ?>"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo-sm.svg" alt="logo" class="logo xs"></a>
+              <!--
               <a class="navbar-brand md d-none d-md-block" href="<?php echo esc_url(home_url()); ?>"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo.svg" alt="logo" class="logo md"></a>
-            </div>
+              -->
 
-            <div class="row">
-            <!-- Offcanvas Navbar -->
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-navbar">
-              <div class="offcanvas-header bg-light">
-                <span class="h5 mb-0"><?php esc_html_e('Menu', 'bootscore'); ?></span>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div class="offcanvas-body">
-                <!-- Bootstrap 5 Nav Walker Main Menu -->
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars"></i><span class="visually-hidden-focusable">Menu</span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarNav">
                 <?php
                 wp_nav_menu(array(
                   'theme_location' => 'main-menu',
                   'container' => false,
                   'menu_class' => '',
                   'fallback_cb' => '__return_false',
-                  'items_wrap' => '<ul id="bootscore-navbar" class="navbar-nav ms-auto %2$s">%3$s</ul>',
+                  'items_wrap' => '<ul id="bootscore-navbar" class="navbar-nav ms-auto%2$s">%3$s</ul>',
                   'depth' => 2,
                   'walker' => new bootstrap_5_wp_nav_menu_walker()
                 ));
                 ?>
                 <!-- Bootstrap 5 Nav Walker Main Menu End -->
               </div>
-            </div>
-            </div>
 
 
             <div class="header-actions d-flex align-items-center">
@@ -103,9 +98,9 @@
               </button>
 
               <!-- Navbar Toggler -->
-              <button class="btn btn-outline-secondary d-lg-none ms-1 ms-md-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-navbar" aria-controls="offcanvas-navbar">
+              <!--<button class="btn btn-outline-secondary d-lg-none ms-1 ms-md-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-navbar" aria-controls="offcanvas-navbar">
                 <i class="fa-solid fa-bars"></i><span class="visually-hidden-focusable">Menu</span>
-              </button>
+              </button>-->
 
             </div><!-- .header-actions -->
 
