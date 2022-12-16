d2febfb20b6df7eeaa18f0e58e5f6b18f078bb1a
Simon <ad7588@coventry.ac.uk>
2022-12-16 10:24:42 +0000

    search accessibility labels

--- searchform.php
+++ searchform.php
@@ -1,6 +1,5 @@
 <!-- Search Button Outline Secondary Right -->
 <form class="searchform input-group" method="get" action="<?php echo esc_url(home_url('/')); ?>">
-  <label for="site-search" hidden>Search</label>
-  <input type="text" id="site-search" name="s" class="form-control" placeholder="<?php _e('Search', 'bootscore'); ?>">
-  <button type="submit" class="input-group-text btn btn-outline-primary"><i class="fa-solid fa-magnifying-glass"></i><span class="visually-hidden-focusable">Search</span></button>
+  <input type="text" name="s" class="form-control" placeholder="<?php _e('Search', 'bootscore'); ?>">
+  <button aria-label="Search" aria-labelledby="search" type="submit" class="input-group-text btn btn-outline-primary"><i class="fa-solid fa-magnifying-glass"></i><span class="visually-hidden-focusable">Search</span></button>
 </form>
