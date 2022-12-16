c362116d3c5ac8f9da45b4063e3651b45fade8b5
Simon <ad7588@coventry.ac.uk>
2022-12-16 10:18:41 +0000

    adding label to searchform

--- searchform.php
+++ searchform.php
@@ -1,5 +1,6 @@
 <!-- Search Button Outline Secondary Right -->
 <form class="searchform input-group" method="get" action="<?php echo esc_url(home_url('/')); ?>">
-  <input type="text" name="s" class="form-control" placeholder="<?php _e('Search', 'bootscore'); ?>">
+  <label for="site-search" hidden>Search</label>
+  <input type="text" id="site-search" name="s" class="form-control" placeholder="<?php _e('Search', 'bootscore'); ?>">
   <button type="submit" class="input-group-text btn btn-outline-primary"><i class="fa-solid fa-magnifying-glass"></i><span class="visually-hidden-focusable">Search</span></button>
 </form>
