02c21ad49bf92ba6fc3ddd6e8fd9fe698444a8e3
Simon <ad7588@coventry.ac.uk>
2023-01-24 16:00:34 +0000

    added another font weight

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
   <!-- Onion-Location Header -->
   <meta http-equiv="onion-location" content="http://testing.onion" />
   <!-- Google font -->
-  <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,300,400,700" rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,300,400,700,800" rel='stylesheet' type='text/css'>
   <!-- Favicons -->
   <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/favicon-32x32.png">
