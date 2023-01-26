39a685085719d8dca06159e78f7b1287f46390ca
Simon <ad7588@coventry.ac.uk>
2023-01-26 13:50:48 +0000

    removing modified date from posts

--- functions.php
+++ functions.php
@@ -194,3 +194,31 @@ function rewrite_permalinks($content) {
 	$content = preg_replace('/https?:\/\/postdigitalcultures.org/', 'http://testing.onion', $content);
 	return $content;
 }
+
+/**
+* Prints HTML with meta information for the current post-date/time.
+* Overwrites existing ./inc/template-tags.php function
+*/
+  function bootscore_date() {
+    $time_string = '<time class="entry-date whatever published updated" datetime="%1$s">%2$s</time>';
+    // if (get_the_time('U') !== get_the_modified_time('U')) {
+    //   $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time> <span class="time-updated-separator">/</span> <time class="updated" datetime="%3$s">%4$s</time>';
+    // }
+
+    $time_string = sprintf(
+      $time_string,
+      esc_attr(get_the_date(DATE_W3C)),
+      esc_html(get_the_date()),
+      //esc_attr(get_the_modified_date(DATE_W3C)),
+      //esc_html(get_the_modified_date())
+    );
+
+    $posted_on = sprintf(
+      /* translators: %s: post date. */
+      '%s',
+      '<span rel="bookmark">' . $time_string . '</span>'
+    );
+
+    echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+
+  }
